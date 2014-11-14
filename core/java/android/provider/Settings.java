482ccaa7e8c210f1e8dc6474f34cf2976f1853f0
Darescu Ionut <ionut.darescu@gmail.com>
2014-11-14 04:24:58 -0500

    AdBlocker 2/3
    Change-Id: I4bf5f55021cf234e47cf344d0183b24b1550ca4e
    credit: tsubus

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3604,6 +3604,12 @@ public final class Settings {
         public static final String T9_SEARCH_INPUT_LOCALE = "t9_search_input_locale";
 
         /**
+         * Disable ads (HFM)
+         * @hide
+         */
+        public static final String HFM_DISABLE_ADS = "hfm_disable_ads";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
